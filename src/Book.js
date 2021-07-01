b8be35f1acac070b1ecf1ed815d277f6ee567061
seshuen <seshu.93@gmail.com>
2021-06-30 23:23:50 -0400

    fix: Add default book cover when book doesnt have cover

--- src/Book.js
+++ src/Book.js
@@ -1,22 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// Create Stateless component
 const Book = (props) => {
   const book = props.book;
+  const bookCover = book.imageLinks
+    ? book.imageLinks.thumbnail
+    : "https://via.placeholder.com/128x193.png?text=No%20Book%20Cover";
   return (
     <li key={book.id}>
       <div className="book">
         <div className="book-top">
-          {book.imageLinks && (
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${book.imageLinks.thumbnail})`,
-              }}
-            ></div>
-          )}
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${bookCover})`,
+            }}
+          ></div>
           <div className="book-shelf-changer">
             <select
               onChange={(event) =>
